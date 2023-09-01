6b1fc157594067e2f3bcc43fd64af9cee65e8c3f
abdalla-ayman <abdallaaymanshandi3@gmail.com>
2023-09-01 14:51:47 +0200

    working on question deletiion faceing obstecles

--- app/dashboard/questions/[id]/page.jsx
+++ app/dashboard/questions/[id]/page.jsx
@@ -1,108 +1,37 @@
-"use client";
-import { useState, useEffect } from "react";
 import getQuestion from "@/lib/question";
-import FormTextarea from "@/app/components/FormTextarea";
-import FormSelect from "@/app/components/FormSelect";
+import Link from "next/link";
 
-let options = [
-  {
-    label: "a",
-    value: "a",
-  },
-  {
-    label: "b",
-    value: "b",
-  },
-  {
-    label: "c",
-    value: "c",
-  },
-  {
-    label: "d",
-    value: "d",
-  },
-];
-
-export default function Question({ params }) {
+export default async function Question({ params }) {
   let id = params.id;
 
-  let [question, setQuestion] = useState("");
-  let [a, setA] = useState("");
-  let [b, setB] = useState("");
-  let [c, setC] = useState("");
-  let [d, setD] = useState("");
-  let [answer, setAnswer] = useState("");
-  let [loading, setLoading] = useState(false);
-  let [error, setError] = useState("");
-
-  useEffect(async () => {
-    let { question } = await getQuestion(id);
-    setQuestion(question.question);
-    setA(question.a);
-    setB(question.b);
-    setC(question.c);
-    setD(question.d);
-    setAnswer(question.answer);
-  }, []);
-
-  let handleSubmit = async (e) => {
-    try {
-      e.preventDefault();
-      let res = await add_question({
-        question,
-        a,
-        b,
-        c,
-        d,
-        answer,
-      });
-
-      let data = await res.json();
-      if (res.ok) {
-        // success logic
-      } else {
-        //failure logic
-        setError(data.message);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
+  let { question } = await getQuestion(id);
   return (
     <div>
-      <div className="my-10">
-        <form className="w-80 m-auto  " onSubmit={handleSubmit}>
-          <h2 className="text-2xl ">اضافة سؤال</h2>
-
-          <FormTextarea
-            value={question}
-            setValue={setQuestion}
-            label="السؤال"
-          />
-          <FormTextarea value={a} setValue={setA} label="الاجابة a" />
-          <FormTextarea value={b} setValue={setB} label="الاجابة b" />
-          <FormTextarea value={c} setValue={setC} label="الاجابة c" />
-          <FormTextarea value={d} setValue={setD} label="الاجابة d" />
-
-          <FormSelect
-            setValue={setAnswer}
-            label="الاجابة الصحيحة"
-            options={options}
-            defaultVal={answer}
-          />
-          {error && <p className="text-red-500 mt-1 mb-3">{error}</p>}
-
-          <div className="flex ">
-            <div className="mx-2">
-              <button className="bg-sky-500 mx-auto block">تعديل</button>
-            </div>
-            <div className="mx-2">
-              <button className="bg-red-500 mx-auto block">حذف</button>
-            </div>
+      {question && (
+        <div className="flex flex-col items-center justify-center h-screen">
+          <h1 className="text-2xl">السؤال:</h1>
+          <h2 className="text-2xl text-start">{question.question}</h2>
+          <h1 className="text-xl mt-8"> الاجابة الصحيحة:</h1>
+          <h3 className="text-lg">{question[question.answer]}</h3>
+          <div className="flex mt-10">
+            <Link
+              href={"/dashboard/questions/edit/" + question._id}
+              className="bg-sky-500 mx-2 block btn"
+            >
+              تعديل
+            </Link>
+            <form
+              action={"/dashboard/questions/delete/" + question._id}
+              method="get"
+            >
+              <input type="text" hidden name="id" defaultValue={question._id} />
+              <button type="submit" className="bg-red-500 mx-2 block btn">
+                حذف
+              </button>
+            </form>
           </div>
-        </form>
-      </div>
+        </div>
+      )}
     </div>
   );
 }
