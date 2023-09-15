f483dabeaa48b73390246c1631be9a580619686c
AHMED-JMD <ahmedabdalrahman706@gmail.com>
2023-09-15 20:33:38 +0200

    added change password pages

--- app/auth/reset_password/page.jsx
+++ app/auth/reset_password/page.jsx
@@ -1,18 +1,18 @@
 "use client";
 import { useState, useEffect } from "react";
 import { useSession, signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
 //components
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-import { Link, Typography } from "@mui/material";
 
 export default function Login() {
-  let [username, setUsername] = useState("");
-  let [password, setPassword] = useState("");
+  let [email, setEmail] = useState("");
   let [error, setError] = useState("");
   let [loading, setLoading] = useState(false);
   let { data: session } = useSession();
-
+  const router = useRouter();
   useEffect(() => {
     console.log(session);
   }, [session]);
@@ -20,13 +20,7 @@ export default function Login() {
   const handleSubmit = async (e) => {
     try {
       e.preventDefault();
-      let result = await signIn("credentials", {
-        username,
-        password,
-        redirect: false,
-      });
-      //result.error for error
-      // }
+      router.push("/auth/reset_password/code");
     } catch (error) {
       console.log(error);
       throw error;
@@ -40,32 +34,21 @@ export default function Login() {
         action={"#"}
         onSubmit={handleSubmit}
       >
-        <h2 className="text-2xl mb-4 text-center">تسجيل الدخول</h2>
+        <h2 className="text-2xl mb-4 text-center">تأكيد الايميل</h2>
         <TextField
           id="outlined-basic"
-          onChange={(e) => setUsername(e.target.value)}
-          label="اسم المستخدم"
+          onChange={(e) => setEmail(e.target.value)}
+          label="ادخل الايميل"
           variant="outlined"
           type="text"
           className="my-5"
           sx={{ my: 1 }}
         />
-        <TextField
-          id="outlined-basic"
-          onChange={(e) => setPassword(e.target.value)}
-          label="كلمة المرور"
-          variant="outlined"
-          type="password"
-          sx={{ my: 1 }}
-        />
-        <Typography>
-          نسيت كلمة السر؟ <Link href={"/auth/reset_password"}>اضغط هنا</Link>
-        </Typography>
         <div>
           {error && <p className="text-red-500 mt-1 mb-3">{error}</p>}
 
           <Button variant="contained" type="submit" sx={{ my: 1 }}>
-            تسجيل الدخول
+            تأكيد الايميل
           </Button>
         </div>
       </form>
