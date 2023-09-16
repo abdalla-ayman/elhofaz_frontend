47428977923ba1710245c58161c276d03a9cc417
abdalla-ayman <abdallaaymanshandi3@gmail.com>
2023-09-16 10:43:41 +0200

    bug fixed

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 import { authOptions } from "./authOptions";
 
-export const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
