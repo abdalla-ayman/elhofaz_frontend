3fc0c04e4cd7e207c89409c0bfffbce26a594b4d
abdalla-ayman <abdallaaymanshandi3@gmail.com>
2023-09-16 10:58:45 +0200

    added

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,46 @@
 import NextAuth from "next-auth";
-import { authOptions } from "./authOptions";
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { login } from "@/lib/users";
+
+const authOptions: NextAuthOptions = {
+  pages: {
+    signIn: "/auth/login",
+  },
+  providers: [
+    CredentialsProvider({
+      async authorize(credentials: any, req) {
+        let { data, code, message } = await login(credentials);
+        console.log(data, code, message);
+
+        if (code == 200) {
+          const name = data.user.name;
+          return { ...data.user, name, accessToken: data.token };
+        } else if (code == 402) {
+          // If you return null then an error will be displayed advising the user to check their details.
+          throw new Error(JSON.stringify(message));
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        } else {
+          throw new Error(JSON.stringify("حدثت مشكلة ما يرجى اعادة المحاولة"));
+        }
+      },
+      credentials: {},
+    }),
+  ],
+  callbacks: {
+    async session({ session, token, user }: any) {
+      session.accessToken = token.accessToken;
+      session.user = token.user;
+      return session;
+    },
+    async jwt({ token, user, ...rest }: any) {
+      if (user) {
+        token = { accessToken: user.accessToken, user };
+      }
+      return token;
+    },
+  },
+};
 
 const handler = NextAuth(authOptions);
 
