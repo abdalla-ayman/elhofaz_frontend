b5c05827643804627693413c9350cdd02e0d9e1b
abdalla-ayman <abdallaaymanshandi3@gmail.com>
2023-09-24 14:57:57 +0200

    fixed bug

--- app/user/profile/page.jsx
+++ app/user/profile/page.jsx
@@ -1,18 +1,25 @@
+"use client"
+
 import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
 import { Container } from "@mui/material";
 
 export default function page() {
   let { data: session } = useSession();
-  return <Container     sx={{
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    minHeight: "100vh",
-    py: 5,
-    paddingTop: 12,
-  }}>
+  return (
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        minHeight: "100vh",
+        py: 5,
+        paddingTop: 12,
+      }}
+    >
+      
 
 
-  </Container>;
+    </Container>
+  );
 }
