6297a93da76eb0322873712de232c1601e9e8083
AHMED-JMD <ahmedabdalrahman706@gmail.com>
2023-12-03 08:19:39 +0200

    Checking again deploy action

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-cd /home/ahmed3310/Projects/elhofaz_frontend/ && git pull && npm install && pm2 restart elhofaz_frontend && echo 'deployment succeeded'
+cd /home/ahmed3310/Projects/elhofaz_frontend/
+git pull
+npm install
+pm2 restart elhofaz_frontend
+echo 'deployment succeeded'
