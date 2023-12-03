08ddd16467eac9971c4b9c9d0c15e10dffe35b0f
Mustafa Balila <mohammedbalila11@gmail.com>
2023-12-03 08:17:31 +0200

    Fixed GH action deploy file

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-cd /home/ahmed3310/Projects/elhofaz_frontend/
-git pull
-npm install
-pm2 restart elhofaz_frontend
-echo 'deployment succeeded'
+cd /home/ahmed3310/Projects/elhofaz_frontend/ && git pull && npm install && pm2 restart elhofaz_frontend && echo 'deployment succeeded'
