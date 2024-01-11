6bb3ea4a22370f2a461fe2d8f9ad58d4eee80ff9
MonzerIdris <monzeridris01@gmail.com>
2024-01-11 21:42:00 +0400

    dddd

--- app/events/page.jsx
+++ app/events/page.jsx
@@ -1,53 +1,53 @@
-// "use client";
-// import { useState, useEffect } from "react";
-// import { useSession } from "next-auth/react";
+"use client";
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
 
-// import Loading from "@/app/components/Loading";
-// import Alert from "@/app/components/Alert";
-// import Box from "@mui/material/Box";
+import Loading from "@/app/components/Loading";
+import Alert from "@/app/components/Alert";
+import Box from "@mui/material/Box";
 
-// import Heading from "./heading";
-// import RecitingCorrection from "./RecitingCorrection";
-// import CharitableFunds from './charitableFund'
+import Heading from "./heading";
+import RecitingCorrection from "./RecitingCorrection";
+import CharitableFunds from './charitableFund'
 
-// export default function Contact() {
-//   // const [faq, setFAQ] = useState([]);
-//   const [loading, setLoading] = useState(false);
-//   const [success, setSuccess] = useState("");
-//   const [error, setError] = useState("");
-//   let { data: session } = useSession();
+export default function Contact() {
+  // const [faq, setFAQ] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState("");
+  const [error, setError] = useState("");
+  let { data: session } = useSession();
 
   
-//   // useEffect(() => {
-//   //   (async function () {
-//   //     let res = await getFAQ();
-//   //     setFAQ(res.data);
-//   //   })();
-//   // }, []);
+  // useEffect(() => {
+  //   (async function () {
+  //     let res = await getFAQ();
+  //     setFAQ(res.data);
+  //   })();
+  // }, []);
 
-//   return (
-//     <Box
-//       sx={{
-//         minHeight: "100vh",
-//         pt: 5,
-//         backgroundColor: "#cba346",
-//         color: "white",
-//       }}
-//     >
-//       <Heading />
-//       <RecitingCorrection />
-//       <CharitableFunds />
-//       {error && (
-//         <Alert severity="error" close={() => setError("")} message={error} />
-//       )}
-//       {success && (
-//         <Alert
-//           message={success}
-//           severity="success"
-//           close={() => setSuccess("")}
-//         />
-//       )}
-//       <Loading loading={loading} text={"جاري التحميل"} />
-//     </Box>
-//   );
-// }
+  return (
+    <Box
+      sx={{
+        minHeight: "100vh",
+        pt: 5,
+        backgroundColor: "#cba346",
+        color: "white",
+      }}
+    >
+      <Heading />
+      <RecitingCorrection />
+      <CharitableFunds />
+      {error && (
+        <Alert severity="error" close={() => setError("")} message={error} />
+      )}
+      {success && (
+        <Alert
+          message={success}
+          severity="success"
+          close={() => setSuccess("")}
+        />
+      )}
+      <Loading loading={loading} text={"جاري التحميل"} />
+    </Box>
+  );
+}
